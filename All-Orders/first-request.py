ca37cd1057e756c49267531db2b547553c6620e5
hibayoussef <59682962+hibayoussef@users.noreply.github.com>
2021-06-09 15:09:08 +0300

    Handle stop words file

--- All-Orders/first-request.py
+++ All-Orders/first-request.py
@@ -1,11 +1,19 @@
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-
-stopwords=[]
+import os
+stopwords = []
 # file is object of file
-with open("../Files/stop words.txt", 'r') as file:
+with open("C:/Users/Super/Desktop/IR/homework/Lab4/IR Homework/stop words.txt", 'r') as file:
     for word in file:
-        word = word.split('/n')
+        word = word.split('\n')
         stopwords.append(word[0])
 
+print('stopwords', stopwords)
+
+# import os
+#
+# with os.scandir('../Files/stop words.txt') as entries:
+#     for entry in entries:
+#         stopwords.append(entry[0])
+#         print(entry)
