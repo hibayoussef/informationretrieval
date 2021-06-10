61186d04309be6f17d1d057b67afd631cfad8e5a
hibayoussef <59682962+hibayoussef@users.noreply.github.com>
2021-06-10 20:13:42 +0300

    Handling stop words

--- All-Orders/first-request.py
+++ All-Orders/first-request.py
@@ -1,34 +1,24 @@
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
 import os
 
+stop_words_list = stopwords.words('english')
+additional_stopwords = []
 
-# 1-Here we take words from stop words file and save it inside
-stopwords = []
 with open("C:/Users/Super/Desktop/IR/homework/Lab4/IR Homework/stop words.txt", 'r') as file:
-    for stop_word in file:
-        stop_word = stop_word.split('\n')
-        stopwords.append(stop_word[0])
+    for word in file:
+        word = word.split('\n')
+        additional_stopwords .append(word[0])
 
-print('stopwords in file are:', stopwords)
+stop_words_list += additional_stopwords
+
+dir_path = 'C:/Users/Super/Desktop/IR/homework/Lab4/corpus/corpus/'
+save_dir = "C:/Users/Super/Desktop/IR/homework/Files_Without_SW/"
+
+for document in os.listdir(dir_path):
+    with open(dir_path + document, "r") as reader:
+        save_file = open(save_dir + document, 'w')
+        text = reader.read()
+        cleaned = [word for word in text.split() if (word not in stop_words_list)]
+        save_file.writelines(["%s\n" % item for item in cleaned])
 
 
-# 2-read each file inside corpus dictionary and print the name of file and calculate the number of files
-documents = os.scandir('C:/Users/Super/Desktop/IR/homework/Lab4/corpus/corpus')
-words_In_document = []
-totalDocuments = 0
-for document in documents:
-    with open(document, 'r') as doc:
-        for line in doc:
-            # print the content of each alone file as line
-            print('document name:', document.name, line)
-            for word in line.split():
-                # print words that found in all files without separates
-                words_In_document.append(word)
-
-    totalDocuments += 1
-
-# The contents of all files without separates
-print('words_In_document:', words_In_document)
-print('totalDocuments:', totalDocuments)
